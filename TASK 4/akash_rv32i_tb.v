78078d355bc137b676cac240fa294e805706648f
Akash-V-Kashyap <akashvkashyap@gmail.com>
2025-01-24 15:51:35 +0530

    TASK 4 Complete

---
+++ TASK 4/akash_rv32i_tb.v
@@ -0,0 +1,23 @@
+module akash_rv32i_tb;
+
+reg clk,RN;
+wire [31:0]WB_OUT,NPC;
+
+akash_rv32i rv32(clk,RN,NPC,WB_OUT);
+
+
+always #3 clk=!clk;
+
+initial begin 
+RN  = 1'b1;
+clk = 1'b1;
+
+$dumpfile ("akash_rv32i.vcd"); //by default vcd
+$dumpvars (0, akash_rv32i_tb);
+  
+  #5 RN = 1'b0;
+  
+  #300 $finish;
+
+end
+endmodule
